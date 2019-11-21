ca122f9f881164203294c7eb1d316b06e906333f
jjan0221 <jjan0221@sookmyung.ac.kr>
2019-11-21 15:20:13 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,19 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30] = "happy C programming";
+	int i;
+	char input[100];
+	FILE *fp;
 	
-	printf("문자열\"%s \"의 길이 : %i", str, strlen(str));
+	fp = fopen("sample.txt", "w");
+	for(i=0;i<3;i++)
+	{
+		printf("input a word: ");
+		scanf("%s", input);
+		fprintf(fp,"$s\n", input);
+		
+	}
 	
+	fclose(fp);
 	return 0;
 }
